e0eb7d4189322f34bfc038261df03e93e19da1ce
Matias Ifran <m.ifran@santafeciudad.gov.ar>
2023-11-08 21:50:53 -0300

    new test but not working

--- tests/Feature/UserEndPointsTest.php
+++ tests/Feature/UserEndPointsTest.php
@@ -9,6 +9,8 @@ use App\Models\User;
 
 class UserEndPointsTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testGetAllUsers()
     {
         $user = User::factory()->create(); 
@@ -28,9 +30,9 @@ class UserEndPointsTest extends TestCase
         ]);
     }
 
-   /* public function testGetUserById()
+    /*public function testGetUserByIdWithValidUserId()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create(); 
         $headers = [
             'Accept'        =>  'application/json',
             'Content-Type'  => 'application/json',
@@ -38,13 +40,28 @@ class UserEndPointsTest extends TestCase
 
         $response = $this->actingAs($user)
             ->withHeaders($headers)
-            ->json('GET','/user/' . $user->id);
+            ->get(route('user.getById', ['userId' => $user->id]));
 
         $response->assertStatus(200);
-        $response->assertJsonStructure([
-            'status',
-            'user',
-        ]);
+    }
+
+    public function testGetUserByIdWithInvalidUserId()
+    {
+        // Autenticar al usuario
+        $this->actingAs(User::factory()->create());
+
+        $headers = [
+            'Accept'        =>  'application/json',
+            'Content-Type'  => 'application/json',
+        ];
+
+        $response = $this->withHeaders($headers)
+                         ->get(route('user.getById', ['userId' => 12345])); // Un ID de usuario que no existe
+
+        $response->assertStatus(404)
+            ->assertJson([
+                'error' => 'Usuario no encontrado',
+            ]);
     }
     
     public function testGetNonExistentUser()
