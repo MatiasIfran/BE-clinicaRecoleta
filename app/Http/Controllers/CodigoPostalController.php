39da26dffd2d482619f0ba0ffa500befaf2f0b20
Matias Ifran <m.ifran@santafeciudad.gov.ar>
2023-12-11 23:27:17 -0300

    new swagger

--- app/Http/Controllers/CodigoPostalController.php
+++ app/Http/Controllers/CodigoPostalController.php
@@ -12,6 +12,49 @@ class CodigoPostalController extends Controller
         $this->middleware('auth:sanctum');
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/cp/all",
+     *      security={{"apiAuth":{}}},
+     *      operationId="getAllCodigosPostales",
+     *      tags={"Codigo Postal"},
+     *      summary="Obtener todos los códigos postales",
+     *      description="Retorna una lista de todos los códigos postales disponibles",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operación exitosa",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="status",
+     *                  type="boolean",
+     *              ),
+     *              @OA\Property(
+     *              type="array",
+     *              property="codigos_postales",
+     *              @OA\Items(
+     *                  type="object",
+     *                      @OA\Property(
+     *                          property="id",
+     *                          type="number",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="codigo",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="descripcion",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="usuario",
+     *                          type="string",
+     *                      ),
+     *                  )
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function allCodigoPostal()
     {
         $codigosPostales = CodigoPostal::all();
@@ -22,6 +65,55 @@ class CodigoPostalController extends Controller
         return response()->json($data, 200);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/cp/id/{codigoPostalId}",
+     *      security={{"apiAuth":{}}},
+     *      operationId="getCodPostById",
+     *      tags={"Codigo Postal"},
+     *      summary="Obtener codigo postal por Id",
+     *      description="Retorna un array del codigo postal disponible",
+     *      @OA\Parameter(
+     *         in="path",
+     *         name="codigoPostalId",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operación exitosa",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="status",
+     *                  type="boolean",
+     *              ),
+     *              @OA\Property(
+     *              type="array",
+     *              property="codigos_postales",
+     *              @OA\Items(
+     *                  type="object",
+     *                      @OA\Property(
+     *                          property="id",
+     *                          type="number",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="codigo",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="descripcion",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="usuario",
+     *                          type="string",
+     *                      ),
+     *                  )
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function getCodPostById($codigoPostalId)
     {
         $codigoPostal = CodigoPostal::find($codigoPostalId);
@@ -41,6 +133,55 @@ class CodigoPostalController extends Controller
         return response()->json($data, 200);
     }
 
+     /**
+     * @OA\Get(
+     *      path="/api/cp/{codigoPostalNumber}",
+     *      security={{"apiAuth":{}}},
+     *      operationId="getCodPostByCodigo",
+     *      tags={"Codigo Postal"},
+     *      summary="Obtener codigo postal por numero",
+     *      description="Retorna un array del codigo postal disponible",
+     *      @OA\Parameter(
+     *         in="path",
+     *         name="codigoPostalNumber",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operación exitosa",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="status",
+     *                  type="boolean",
+     *              ),
+     *              @OA\Property(
+     *              type="array",
+     *              property="codigos_postales",
+     *              @OA\Items(
+     *                  type="object",
+     *                      @OA\Property(
+     *                          property="id",
+     *                          type="number",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="codigo",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="descripcion",
+     *                          type="string",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="usuario",
+     *                          type="string",
+     *                      ),
+     *                  )
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function getCodPostByCodigo($codigo)
     {
         $codigoPostal = CodigoPostal::where('codigo', $codigo)->first();
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+* @OA\Info(
+*    title="Api's Clinica Recoleta", 
+*    version="1.0")
+* @OA\Server(url="http://localhost:8000")
+* @OA\SecurityScheme(
+*     type="http",
+*     description="Login with email and password to get the authentication token",
+*     name="Token based Based",
+*     in="header",
+*     scheme="bearer",
+*     bearerFormat="JWT",
+*     securityScheme="apiAuth",
+* )
+*/
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
