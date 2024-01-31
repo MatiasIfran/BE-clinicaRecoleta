d9f0c6a12950cf4e5993b85d923a21b32537f3e7
Matias Ifran <m.ifran@santafeciudad.gov.ar>
2024-01-31 20:42:11 -0300

    fix in database Pacientes

--- database/migrations/2023_11_24_201233_pacientes.php
+++ database/migrations/2023_11_24_201233_pacientes.php
@@ -15,22 +15,25 @@ class Pacientes extends Migration
     {
         Schema::create('pacientes', function (Blueprint $table) {
             $table->id();
-            $table->string('Nombre', 50)->nullable(false);
-            $table->string('Apellido', 50)->nullable(false);
-            $table->date('FechaNacimiento')->nullable();
-            $table->string('Genero', 10)->nullable();
+            $table->string('Nombre')->nullable(false);
+            $table->string('Apellido')->nullable(false);
             $table->string('Direccion', 100)->nullable();
             $table->string('CodPos', 5)->nullable();
-            $table->string('Telefono', 15)->nullable();
-            $table->string('Mail', 100)->nullable();
-            $table->unsignedBigInteger('TipoDocumento')->nullable(false);
+            $table->unsignedBigInteger('TipoDocumento')->nullable(false)->default('0000');
             $table->unsignedBigInteger('NumDocumento')->nullable(false)->unique();
-            $table->unsignedBigInteger('Cuit')->nullable();
-            $table->unsignedBigInteger('NumAfiliado')->nullable();
-            $table->unsignedBigInteger('Empres')->nullable();
+            $table->string('Telefono', 100)->nullable();
+            $table->date('FechaNacimiento')->nullable();
+            $table->date('FechaIngreso')->nullable();
+            $table->date('FechaCarga')->nullable();
+            $table->string('NumAfiliado')->nullable();
+            $table->string('Empres')->nullable();
+            $table->string('Antecedentes')->nullable();
+            $table->string('Iva')->nullable();
+            $table->string('Cuit')->nullable();
             $table->unsignedBigInteger('DetaPlan')->nullable();
-            $table->unsignedBigInteger('Plan')->nullable();
-            $table->unsignedBigInteger('Antecedentes')->nullable();
+            $table->string('Plan')->nullable();
+            $table->string('Mail', 100)->nullable();
+            $table->string('Genero', 10)->nullable();
             $table->timestamp('updated_at')->useCurrent();
             $table->timestamp('created_at')->default(now());            
             $table->string('usuario', 50)->nullable(false);
